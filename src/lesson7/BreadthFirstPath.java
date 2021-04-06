79329a55f5a45400547fc66b94d893b467122ceb
Kashevar128 <steve128@yandex.ru>
2021-04-06 21:52:35 +0300

    added Home Work lesson7

--- src/lesson7/BreadthFirstPath.java
+++ src/lesson7/BreadthFirstPath.java
@@ -2,48 +2,26 @@ package lesson7;
 
 import java.util.LinkedList;
 
-public class BreadthFirstPath {
-    private boolean[] marked;
-    private int[] edgeTo;
-    private int source;
+public class BreadthFirstPath extends FirstPath {
 
     public BreadthFirstPath(Graph g, int source) {
-        this.source = source;
-        marked = new boolean[g.getVertexCount()];
-        edgeTo = new int[g.getVertexCount()];
+        super(g, source);
         bfs(g, source);
     }
 
     private void bfs(Graph g, int source) {
         LinkedList<Integer> queue = new LinkedList<>();
         queue.addLast(source);
-        marked[source] = true;
+        getMarked()[source] = true;
         while (!queue.isEmpty()){
             int vertex = queue.removeFirst();
             for (int w : g.getAdjList(vertex)) {
-                if(!marked[w]){
-                    marked[w] = true;
-                    edgeTo[w] = vertex;
+                if(!getMarked()[w]){
+                    getMarked()[w] = true;
+                    getEdgeTo()[w] = vertex;
                     queue.addLast(w);
                 }
             }
         }
-    }
-
-    public boolean hasPathTo(int w) {
-        return marked[w];
-    }
-
-    public LinkedList<Integer> pathTo(int w) {
-        if (!hasPathTo(w)) {
-            return null;
-        }
-        LinkedList<Integer> stack = new LinkedList<>();
-        int vertex = w;
-        while (vertex != source) {
-            stack.push(vertex);
-            vertex = edgeTo[vertex];
-        }
-        return stack;
     }
 }
