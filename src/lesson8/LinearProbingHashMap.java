71e58bf601fc4df35353c08e6b828ffb1e3a7120
Мстислав В. Виноградов <Vinogradov_M@office.kontur-niirs.ru>
2021-04-09 14:59:00 +0300

    Home Work Lesson update 002

--- src/lesson8/LinearProbingHashMap.java
+++ src/lesson8/LinearProbingHashMap.java
@@ -43,21 +43,27 @@ public class LinearProbingHashMap<Key, Value> {
 
     public void checkLoadFactor() {
         double loadFactor = (double) size / capacity;
+        int oldCapacity = capacity;
         if (loadFactor > LOAD_FACTOR) {
             capacity = (int) (capacity * 2);
             int count = 0;
-            while (count > 2) {
+            do {
+                count = 0;
                 for (int j = 1; j <= capacity; j++) {
                     if(capacity % j == 0) {
                         count++;
                     }
                 }
                 if (count != 2) capacity++;
-            }
+            } while(count != 2);
+
             Key[] newKeys = (Key[]) new Object[capacity];
             Value[] newValues = (Value[]) new Object[capacity];
-            for (int i = 0; i < capacity; i++) {
-                int h = hash(keys[i]);
+            for (int i = 0; i < oldCapacity; i++) {
+                int h = 0;
+                if(keys[i] != null){
+                    h = hash(keys[i]);
+                }
                 int step = 1;
                 while (newKeys[h] != null){
                     h = (h+step) % capacity;
@@ -65,12 +71,15 @@ public class LinearProbingHashMap<Key, Value> {
                 newKeys[h] = keys[i];
                 newValues[h] = values[i];
             }
+            keys = newKeys;
+            values = newValues;
         }
     }
 
     public void put(Key key, Value value){
         // проверка на прввышение лоад фактора. Либо просаем эксепшн либо
         // перехиширование на массив размером больше, но простое число
+        checkLoadFactor();
         checkKeyNotNull(key);
         int i = hash(key);
         int step = 1;
@@ -78,6 +87,11 @@ public class LinearProbingHashMap<Key, Value> {
             if(keys[i].equals(key)){
                 values[i]= value;
                 return;
+            }
+            if(keys[i].equals(DELETED)) {
+                keys[i] = key;
+                values[i] = value;
+                size++;
             }
             i = (i+step) % capacity;
         }
@@ -99,4 +113,21 @@ public class LinearProbingHashMap<Key, Value> {
         }
         return null;
     }
+
+    public void deleted(Key key) {
+        checkKeyNotNull(key);
+        int i = hash(key);
+        int step = 1;
+
+        while (keys[i] != null) {
+            if(keys[i].equals(key)) {
+                values[i] = (Value) DELETED;
+                keys[i] = (Key) DELETED;
+                size--;
+                return;
+            }
+            i = (i + step) % capacity;
+        }
+    }
+
 }
--- src/lesson8/Main.java
+++ src/lesson8/Main.java
@@ -5,50 +5,64 @@ import java.util.Random;
 public class Main {
     static Random random;
     public static void main(String[] args) {
-        random = new Random();
+   //     random = new Random();
         ChainingHashMap<Integer, String> chm= new ChainingHashMap<>(7);
 
         chm.put(1, "one");
         chm.put(2, "two");
-        chm.put(4, "three");
-        chm.put(5, "three");
-        chm.put(6, "three");
-        chm.put(7, "three");
-        chm.put(8, "three");
-        chm.put(9, "three");
-        chm.put(10, "three");
-        chm.put(11, "three");
-        chm.put(12, "three");
-        chm.put(13, "three");
-        chm.put(14, "three");
-        chm.put(15, "three");
-        chm.put(16, "three");
+        chm.put(3, "three");
 
-        for (int i = 1; i <= 16; i++) {
-            System.out.println(chm.get(i));
-        }
+        System.out.println(chm.get(1));
+        System.out.println(chm.get(2));
+        System.out.println(chm.get(3));
 
+
+        chm.delete(2);
+
+        System.out.println(chm.get(1));
+        System.out.println(chm.get(2));
+        System.out.println(chm.get(3));
 //
-//        chm.delete(2);
-//
-//        System.out.println(chm.get(1));
-//        System.out.println(chm.get(2));
-//        System.out.println(chm.get(3));
-
 //        for (int i = 0; i < 12; i++) {
 //            chm.put(random.nextInt(100), "");
 //        }
 //
 //        System.out.println(chm);
-//
-//        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>(19);
-//        lphm.put(1, "one");
-//        lphm.put(2, "two");
-//        lphm.put(3, "three");
+
+        System.out.println();
+
+        LinearProbingHashMap<Integer, String> lphm = new LinearProbingHashMap<>(7);
+        lphm.put(1, "one");
+        lphm.put(2, "two");
+        lphm.put(3, "three");
+        lphm.put(4, "three");
+        lphm.put(5, "three");
+        lphm.put(6, "three");
+        lphm.put(7, "three");
+        lphm.put(8, "three");
+        lphm.put(9, "three");
+        lphm.put(10, "three");
+
 //        lphm.put(2, "twosdfsdf");
 //        System.out.println(lphm.get(2));
-//
 
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(lphm.get(i));
+        }
+
+        lphm.deleted(5);
+
+        System.out.println();
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(lphm.get(i));
+        }
+
+        lphm.put(5, "hoy");
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(lphm.get(i));
+        }
 
     }
 }
